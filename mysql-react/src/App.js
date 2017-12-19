4a8ed522dad0fbade3601bcd934be4fb27c11692
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-12-19 15:23:15 +0700

    #1

--- mysql-react/src/App.js
+++ mysql-react/src/App.js
@@ -3,16 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
   state = { data: null, isLoading: false }
   componentDidMount() {
-    fetch('https://192.168.2.127:8000')
-    .then(result => this.setState({ data: result }))
+    
   }
   render() {
-      const { data, isLoading } = this.state
       return (
           <div>
-              <h1>Users</h1>
-              {isLoading && <div>Loading...</div>}
-              <h1> {data} </h1>
+              <h1>Test</h1>
+              <input type="text" name="code"></input>
           </div>
       )
   }
